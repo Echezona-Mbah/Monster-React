fe916818cfabd9837cf2b4d0d0a363bd9469bcb5
Echezona-Mbah <cclassico998@gmail.com>
2024-08-21 09:43:36 -0700

    first commit

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -10,6 +10,6 @@ export const SearchBox = ({placeholder,headleCharge}) => (
     type='search'
     placeholder={placeholder}
     onChange={headleCharge}
-    //jjjjjjjwesfesrfsddddd
+    //jjjjjj
   />
 )
